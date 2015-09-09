42aae8fe15919381f41c6fa7f1909efb826327c8
kirillmagadan <kirill@reefstudio.ru>
2015-09-09 10:18:36 +0300

    version 1

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    // slider
+    $('.header--slider').smSlider({
+        transition : 'fader',
+        duration : 500,
+        autoPlay : false,
+        autoArr : false,
+        pagination : true
+    });
+
+    // fancy
+    $('.fancy').fancybox();
+
+
+});
