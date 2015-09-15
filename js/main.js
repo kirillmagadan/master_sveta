3ef6de4671d6175a5b466a694e6f50b72e62c322
kirillmagadan <kirill@reefstudio.ru>
2015-09-15 18:07:00 +0300

    version 3
    directory

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     // slider
-    $('.header--slider').smSlider({
+   /* $('.header--slider').smSlider({
         transition : 'fader',
         duration : 500,
         autoPlay : false,
@@ -10,7 +10,21 @@ $(document).ready(function(){
     });
 
     // fancy
-    $('.fancy').fancybox();
+    $('.fancy').fancybox();*/
+
+    $(function() {
+        $( "#slider-range" ).slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [ 75, 300 ],
+            slide: function( event, ui ) {
+                $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            }
+        });
+        $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+            " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    });
 
 
 });
